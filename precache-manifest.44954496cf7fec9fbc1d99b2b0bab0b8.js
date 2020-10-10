3ca7541bb5bd3e8d8c1544d27ba7110d7f0b7ea6
Alex Fuss <alexfuss2@gmail.com>
2020-10-10 14:06:11 -0400

    Updates

---
+++ precache-manifest.44954496cf7fec9fbc1d99b2b0bab0b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5024daa0c1fa247b35fdbf356136bc5f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "26e7b94ae471d018379e",
+    "url": "/static/css/main.5f75ffa3.chunk.css"
+  },
+  {
+    "revision": "0e8713ad46056ea5deab",
+    "url": "/static/js/2.fe669e20.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.fe669e20.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26e7b94ae471d018379e",
+    "url": "/static/js/main.642b212f.chunk.js"
+  },
+  {
+    "revision": "7a235cb05d8669b2d22d",
+    "url": "/static/js/runtime-main.7ba75982.js"
+  }
+]);
